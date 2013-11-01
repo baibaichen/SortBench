81b7839862558abd3f03946568775b02e3fcd38e
baibaichen <baibaichen@gmail.com>
2013-11-01 11:23:23 +0800

    update to template version, so that we can compare various primitive type

--- swaptest.cc
+++ swaptest.cc
@@ -5,11 +5,11 @@
 #include <algorithm>
 #include <functional>
 #include <iostream>
+#include <cstdint>
 
 using namespace std;
 
-typedef int DType;
-
+template <typename DType>
 inline void Bentleyswap(DType*x,int i, int j)
 {  
   DType t = x[i];
@@ -33,6 +33,19 @@ void test_iter_swap(_RanIt _First, _RanIt _Last)
     std::iter_swap(_First++,--_Last);
 }
 
+
+template <typename _RanIt>
+int test_compare(_RanIt _First, _RanIt _Last)
+{ // [_Frist,_Last)
+  register int temp = 0;
+  while(_First < _Last){
+    if(*_First++ <  *--_Last)
+      temp++;
+  } 
+  return temp;
+}
+
+template <typename DType>
 void test_Bentleyswap(DType*x,int i, int u)
 { // [l,u]
   while (i < u)
@@ -40,42 +53,57 @@ void test_Bentleyswap(DType*x,int i, int u)
 }
 const int maxIter = 100;
 
+enum method_t
+{
+  itertor_swap  = 0,
+  array_swap,
+  value_compare,
+
+  max_method_count,
+};
+
+const char* methodName[max_method_count] = 
+{ 
+  " iter_swap",
+  "array_swap",
+  "  compare "
+};
+
+double timesPerRun[max_method_count][maxIter];
+
+template <typename VType>
 static void bench(int iter,int size)
 {
-  int * data = (int *) malloc(sizeof(int) * size);
+  VType * data = (VType *) malloc(sizeof(VType) * size);
   generate_test_datas(data,data+size);
 
-  const char* methodName[2] = 
-  { " iter_swap ",
-    "Bentleyswap"};
-  
-  double timesPerRun[2][maxIter];
-  const int allRuns = iter * 2;
-  int curentRun = 0;
 
+  const int allRuns = iter * max_method_count;
+  int curentRun = 0;
+  int xxx;
   for (int iter_i = 0 ; iter_i < iter; iter_i++)
-    for(int i = 0; i < 2;i++){
-      cerr << "run ["  << ++curentRun << '/' << allRuns << "]\n";
+    for(int i = 0; i < max_method_count;i++){
+
+      cerr << "run ["  << ++curentRun << '/' << allRuns << "]";
       clock_t t1 = clock();
-      if(i)
-        test_Bentleyswap(data,0,size-1);
-      else
+      switch (i)
+      {
+      case itertor_swap:
         test_iter_swap(data,data+size);
+        break;
+      case array_swap:
+        test_Bentleyswap(data,0,size-1);
+        break;
+      case value_compare:
+        xxx = test_compare(data,data+size);
+        break;
+      }
       clock_t t2 = clock();
+      if(i == value_compare)
+        cerr <<" = "<< xxx ;
+      cerr <<'\n';
       timesPerRun[i][iter_i]= (double)(t2-t1)/CLOCKS_PER_SEC;
     }
-
-  cout<< " size \t" 
-      << "method\t" 
-      << " time\n";
-  for (int j = 0; j < iter;j++)
-    for(int i = 0;i < 2;i++){
-      cout<<size<<"\t"
-          <<methodName[i]<<"\t"
-          <<timesPerRun[i][j]
-          <<'\n';
-    }
-
   free(data);
 }
 int main(int argc, char *argv[])
@@ -87,5 +115,35 @@ int main(int argc, char *argv[])
     if (argc > 1)    N = atoi(argv[1]);
     if (argc > 2) Iter = atoi(argv[2]);
     Iter = min(Iter,maxIter);
-    bench(Iter,N);
+
+    cout<< " type \t" 
+      << "method\t" 
+      << " time\n";
+    
+    bench<int32_t>(Iter,N);
+    for (int j = 0; j < Iter;j++)
+      for(int i = 0;i < max_method_count;i++){
+        cout<<"int32_t"<<"\t"
+          <<methodName[i]<<"\t"
+          <<timesPerRun[i][j]
+        <<'\n';
+      }
+    
+    bench<int64_t>(Iter,N);
+    for (int j = 0; j < Iter;j++)
+      for(int i = 0;i < max_method_count;i++){
+        cout<<"int64_t"<<"\t"
+          <<methodName[i]<<"\t"
+          <<timesPerRun[i][j]
+        <<'\n';
+      }
+
+    bench<double>(Iter,N);
+    for (int j = 0; j < Iter;j++)
+      for(int i = 0;i < max_method_count;i++){
+        cout<<"double"<<"\t"
+          <<methodName[i]<<"\t"
+          <<timesPerRun[i][j]
+        <<'\n';
+      }
 }
